0e77840acb14e2e9fbafb4d8821b54fdbe18b6be
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-01 18:45:07 +0100

    create some function of app.js page to make our index page fun

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+    $("h1").click(function(){
+        alert("This is Heading tage");
+        alert("I told you this is Heading mate !");
+    });
+
+    $("h2").click(function(){
+        alert("This is Subheading");
+
+    });
+
+    $("img").click(function(){
+        alert("This is an Image");
+        alert("I told you this is my jquery journey image");
+    });
+
+});
