2016bc4ae46c520bad16985c69d25757810fb00a
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-01 20:53:25 +0100

    add more event listener to the code

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
 
 
-    $("h1").click(function(){
-        alert("This is Heading tage");
-        alert("I told you this is Heading mate !");
-    });
+    // $("h1").click(function(){
+    //     alert("This is Heading tage");
+    //     alert("I told you this is Heading mate !");
+    // });
 
     $("h2").click(function(){
         alert("This is Subheading");
 
     });
 
+    $("p").click(function(){
+        alert("This is a paragraph");
+        alert("This is where you write pagagraph");
+    });
+
     $("img").click(function(){
         alert("This is an Image");
         alert("I told you this is my jquery journey image");
+    });
+
+    $("h1").dblclick(function(){
+        alert("The text have been changed");
     });
 
 });
